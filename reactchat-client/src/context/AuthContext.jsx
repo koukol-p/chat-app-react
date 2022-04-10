dceb65ee928e63553c4e7f8207d3bf7414d4dccc
koukol-p <p.koukol13@gmail.com>
2022-04-10 23:32:18 +0200

    gitignore changed

--- reactchat-client/src/context/AuthContext.jsx
+++ reactchat-client/src/context/AuthContext.jsx
@@ -5,6 +5,7 @@ export const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
